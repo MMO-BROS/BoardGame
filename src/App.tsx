2726475fbb69f0ef1333f77c0add63fa4ae3642d
Jessica <jessicachisnall.9122@gmail.com>
2018-10-01 11:39:00 +0100

    My first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import "./App.css";
 class App extends React.Component {
   public render() {
     return (
-      <Stage>
-        <Sprite
-          image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
-          x={100}
-          y={100}
-        />
-      </Stage>
+      <div>
+        <p>Hello world</p>
+        <Stage>
+          <Sprite
+           image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
+            x={100}
+            y={100}
+          />
+        </Stage>
+      </div>
     );
   }
 }
