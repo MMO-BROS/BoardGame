7692680d4bf7203b8d67268718d4ff50349e202c
Anders Jakob Sivesind <ajsivesind@gmail.com>
2018-10-02 15:22:47 +0200

    Lots of stuff, added libraries and such

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,43 +1,21 @@
 import * as React from "react";
-import { MenuItem, Nav, Navbar, NavDropdown, NavItem } from "react-bootstrap";
 import "./App.css";
 
 class Navigation extends React.Component {
   public render() {
     return (
-      <Navbar inverse={true} collapseOnSelect={true}>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#brand">BoardGame</a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              Link
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Link
-            </NavItem>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider={true} />
-              <MenuItem eventKey={3.3}>Separated link</MenuItem>
-            </NavDropdown>
-          </Nav>
-          <Nav pullRight={true}>
-            <NavItem eventKey={1} href="#">
-              Link Right
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Link Right
-            </NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <ul className="nav nav-pills nav-fill">
+        <li className="nav-item">
+          <a className="nav-link" href="">
+            Link1
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="">
+            Link2
+          </a>
+        </li>
+      </ul>
     );
   }
 }
